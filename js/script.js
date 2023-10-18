3ce8e02ad99ea1e7cc174f78fa3049962921c7ab
davidelunetta <davidelunetta.27@gmail.com>
2023-10-18 14:43:54 +0200

    updated script with game

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const gridContainer = document.getElementById("grid");
+const generateGridButton = document.getElementById("GridButton");
+
+generateGridButton.addEventListener("click", () => {
+  generateGrid();
+});
+
+function generateGrid() {
+  gridContainer.innerHTML = "";
+
+  for (let row = 0; row < 10; row++) {
+    for (let col = 0; col < 10; col++) {
+      const cell = document.createElement("div");
+      cell.className = "cell";
+      cell.textContent = (row * 10 + col + 1).toString();
+      cell.addEventListener("click", (event) => {
+        cell.style.backgroundColor = "blue";
+        console.log("Hai cliccato sulla cella " + cell.textContent);
+      });
+      gridContainer.appendChild(cell);
+    }
+  }
+}
